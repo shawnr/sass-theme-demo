18c6caff4b1fe906a35071a78c3b072819f46eab
Shawn Rider <shawn@shawnrider.com>
2016-06-14 11:34:19 -0700

    Stubbed out theme toggle.
    Change-Id: Id9155c52c7ce6b9f3669044e18880b0278212679

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function ( event ) {
+  $('#theme-selector').on( 'change keyup', function() {
+      var selected = this.value;
+      $('body').removeClass();
+      $('body').addClass(selected);
+  });
+});
